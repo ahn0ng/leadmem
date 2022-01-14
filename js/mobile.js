5c9a6cf3b513ea57618eb95016d910956174feda
ahn0ng <71095151+ahn0ng@users.noreply.github.com>
2022-01-14 17:43:35 +0900

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -1,25 +1,42 @@
-
 // 최적화 
 $(function() {
     var a = 0;
     var b = 0;
-    var c = 0;
-    var d = 0;
+    
+    $("#route").css("top",route_position);
+    resize_home()
 
+    $(window).resize(function() {
+        resize_home()
+    });
+    
     $(".drag_bar").click(function(){
         a++;
         b=a%2;
 
         if(b==1){
-            $("#route").stop().animate({"bottom":"8vh"},500,"linear");
+            $("#route").stop().animate({"top":"80px"},500,"linear");
         }else{
-            $("#route").stop().animate({"bottom":"-65vh"},500,"linear");
+            $("#route").stop().animate({"top":drag_top_height},500,"linear");
         }
     });
 
 });
+var screen_height;
+var drag_top_height;
+var route_position
+
+function resize_home(){    
+    screen_height =$(window).height();
+    var route_height = screen_height*0.85;
+    drag_top_height = route_height-0.02;
+    route_position = screen_height-100;
 
 
+
+    $("#route").css("top",route_position);
+    $("#route").css("height",route_height);
+}
 
 $(function() {
     var c = 0;
@@ -37,3 +54,5 @@ $(function() {
         }
     });
 });
+
+
