12089cc565f4f072975a4ffcc2e04fdafa968cc4
ahn0ng <71095151+ahn0ng@users.noreply.github.com>
2022-02-08 20:24:50 +0900

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -6,11 +6,13 @@ var route_position;
 function resize_home(){    
     screen_height =$(window).height();
     var route_height = screen_height*0.85;
+    var menu_height = screen_height*0.95;
     // drag_top_height = route_height-0.02;
     route_position = screen_height-90;
 
     $("#route").css("top",route_position);
     $("#route").css("height",route_height);
+    $("#list").css("height",menu_height);
 }
 
 $(function() {
@@ -38,10 +40,18 @@ $(function() {
     });
 
     // 리스트와 메뉴 열었다 닫기 
+    // 리스트 변수
     var c = 0;
     var d = 0;
+    // 메뉴 변수
     var e = 0;
     var f = 0;
+    // 로그인 변수
+    var g = 0;
+    var h = 0;
+    // 회원가입 변수
+    var i = 0;
+    var j = 0;
 
 
     $("#user_btn").click(function(){
@@ -87,10 +97,17 @@ $(function() {
         d=d*0;
         e=e*0;
         f=f*0;
+        g=g*0;
+        h=h*0;
+        i=i*0;
+        j=j*0;
+        
         $("#user").hide();
         $("#list").hide();
         $("#user_icon").css('background','url("https://img.icons8.com/fluency-systems-filled/48/000000/menu.png") no-repeat center').css('background-size','contain');
         $("#list_icon").css('background','url("https://img.icons8.com/material-outlined/50/000000/ingredients-list.png") no-repeat center').css('background-size','contain');
+        $("#login").hide();
+        $("#register").hide();
 
         }else if(b==1){
             b=b*0;
@@ -103,17 +120,55 @@ $(function() {
         }
     });
 
-// 원본
-    // $("#home_btn").click(function(){
-    //     c=c*0;
-    //     d=d*0;
-    //     e=e*0;
-    //     f=f*0;
-    //     $("#user").hide();
-    //     $("#list").hide();
-    //     $("#user_icon").css('background','url("https://img.icons8.com/fluency-systems-filled/48/000000/menu.png") no-repeat center').css('background-size','contain');
-    //     $("#list_icon").css('background','url("https://img.icons8.com/material-outlined/50/000000/ingredients-list.png") no-repeat center').css('background-size','contain');
-    // });
+    // 로그인
+
+    $("#loginout_btn").click(function(){
+        g++;
+        h=g%2;
+
+        if(h==1){
+            $("#login").show();
+            
+        }else{
+        
+            $("#login").hide();
+        }
+    });
+
+    $("#back_btn").click(function(){
+        g=g*0;
+        h=h*0;
+        $("#login").hide();
+    });
+
+    // 회원가입
+
+    $("#register_btn").click(function(){
+        i++;
+        j=i%2;
+
+        if(j==1){
+            $("#register").show();
+            
+        }else{
+        
+            $("#register").hide();
+        }
+    });
+
+    $("#back_btn2").click(function(){
+        i=i*0;
+        j=j*0;
+        $("#register").hide();
+    });
+
+    $("#login_pg").click(function(){
+        i=i*0;
+        j=j*0;
+        $("#register").hide();
+    });
+
+    
 });
 
 
